2bb8b12926848e394b07d1aede97e2b1cb9dd453
Vivek <vivek.ky9304@gmail.com>
2026-04-27 00:42:40 +0530

    Finding floor square root using Binary search algorithm

--- src/BinarySearch/floor_sqrt.java
+++ src/BinarySearch/floor_sqrt.java
@@ -5,27 +5,20 @@ import java.util.Scanner;
 public class floor_sqrt {
 
     static int floorSqrt(int k) {
-        int root = 0;
-        for (int i = 1; i <= k; i++) {
-            root = i;
-            if (i * i > k) break;
+        int start = 0, end = k;
+        int mid = (start + end) / 2;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (mid * mid > k) end = mid - 1;
+            else if (mid * mid == k) return mid;
+            else start = mid + 1;
         }
-        return root - 1;
-    }
-
-    static int floor_cbrt(int k) {
-        int root = 0;
-        for (int i = 1; i <= k; i++) {
-            root = i;
-            if (i * i * i > k) break;
-        }
-        return root - 1;
+        return mid;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
         System.out.println("floor Square root = " + floorSqrt(k));
-        System.out.println("floor cube root = " + floor_cbrt(k));
     }
 }
