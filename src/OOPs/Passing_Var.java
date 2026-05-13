7e338af89182f9fdd3f874e2e99346a3b1a2586b
Vivek <vivek.ky9304@gmail.com>
2026-05-13 12:46:15 +0530

    Passing variable value through object

---
+++ src/OOPs/Passing_Var.java
@@ -0,0 +1,9 @@
+package OOPs;
+
+public class Passing_Var {
+    public static void main(String[] args) {
+        Plot h = new Plot();
+        h.location = "Mumbai";
+        System.out.print(h.location);
+    }
+}
